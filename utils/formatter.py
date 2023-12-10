90508fe4387905486d67176beba7afd9b5da99bc
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 00:22:03 -0800

    add argument

--- utils/formatter.py
+++ utils/formatter.py
@@ -13,7 +13,7 @@ def format_title(txt: str, max_w=80, padding=5) -> str:
     split_txt = ["#"*max_w] + split_txt + ["#"*max_w]
     return "\n".join(split_txt).upper()
 
-def format_paragraph(txt: str, max_w=80) -> str:
     split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
+def format_paragraph(txt: str, max_w=80, padding=2) -> str:
     split_txt = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
     return "\n".join(split_txt)
