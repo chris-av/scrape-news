6f02e39a477a2864f21b8ffca826c769a0da0f04
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-09 22:02:07 -0800

    wrap title in prefix

--- utils/formatter.py
+++ utils/formatter.py
@@ -6,6 +6,7 @@ def format_title(txt, max_w=80):
     # split text by straight number of characters
     split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
     split_txt = list(map(lambda x: f"{title_prefix} {x}", split_txt))
+    split_txt=  ["#"*max_w] + split_txt + ["#"*max_w]
     return "\n".join(split_txt).upper()
 
 def format_paragraph(txt, max_w=80):
