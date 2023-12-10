d1040ee3e2f1247b5d95e47c86755d4cdb908c95
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 00:57:27 -0800

    rename variables for clarity
    Should not be called padding_x. Could be best described as the space allocated,
    not the padding spaces itself.

--- utils/formatter.py
+++ utils/formatter.py
@@ -4,8 +4,8 @@ paragraph_prefix = "  "
 
 def format_title(txt: str, max_w=80, padding=5) -> str:
     # split text by straight number of characters
-    padding_x = max_w - padding*2
-    split_txt = [txt[i:i+padding_x] for i in range(0, len(txt), padding_x)]
+    padding_x = padding*2
+    space_alloc_x = max_w - padding_x
     split_txt = list(map(lambda x: f"{title_prefix} {x}", split_txt))
     for indx, elem in enumerate(split_txt):
         needed_spaces = max_w - len(title_prefix) - len(elem)
@@ -14,8 +14,9 @@ def format_title(txt: str, max_w=80, padding=5) -> str:
     return "\n".join(split_txt).upper()
 
 def format_paragraph(txt: str, max_w=80, padding=2) -> str:
-    padding_x = max_w - padding*2
-    split_txt = [txt[i:i+padding_x] for i in range(0, len(txt), padding_x)]
+    padding_x = padding*2
+    space_alloc_x = max_w - padding_x
+    split_txt = [txt[i:i+space_alloc_x] for i in range(0, len(txt), space_alloc_x)]
     split_txt = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
     for indx, elem in enumerate(split_txt):
         needed_spaces = max_w - len(paragraph_prefix) - len(elem)
