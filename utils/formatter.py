ee67453f78452dd693ac470082956298f148cdce
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 01:22:04 -0800

    wrap by words, not by character

--- utils/formatter.py
+++ utils/formatter.py
@@ -34,7 +34,25 @@ def format_title(txt: str, max_w=80, padding=5) -> str:
 def format_paragraph(txt: str, max_w=80, padding=2) -> str:
     padding_x = padding*2
     space_alloc_x = max_w - padding_x
-    split_txt = [txt[i:i+space_alloc_x] for i in range(0, len(txt), space_alloc_x)]
+
+    words = txt.split(" ")
+    parag_frame = []
+    line = ""
+    char_count = 0
+
+    for word in words:
+        next_part = f" {word}"
+        if (char_count+len(next_part) <= space_alloc_x):
+            char_count += len(next_part)
+            line += next_part
+        else:
+            parag_frame.append(line.strip())
+            line = ""
+            char_count = len(next_part)
+            line += next_part
+
+    parag_frame.append(line.strip())
+    split_txt = parag_frame
     split_txt = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
     for indx, elem in enumerate(split_txt):
         needed_spaces = max_w - len(paragraph_prefix) - len(elem)
