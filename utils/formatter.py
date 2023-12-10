b14f850b717c8362f994e1b238b4b7f29e76028b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 00:16:21 -0800

    use padding
    calculate padding on x axis and use that to calculate when to split

--- utils/formatter.py
+++ utils/formatter.py
@@ -5,6 +5,8 @@ paragraph_prefix = "--"
 def format_title(txt, max_w=80):
 def format_title(txt: str, max_w=80, padding=5) -> str:
     # split text by straight number of characters
+    padding_x = max_w - padding*2
+    split_txt = [txt[i:i+padding_x] for i in range(0, len(txt), padding_x)]
     split_txt = list(map(lambda x: f"{title_prefix} {x}", split_txt))
     split_txt = ["#"*max_w] + split_txt + ["#"*max_w]
     return "\n".join(split_txt).upper()
