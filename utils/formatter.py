2ca0e52cccfa29abf2161d7cab7430ce82629738
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 00:15:19 -0800

    add types and extra argument

--- utils/formatter.py
+++ utils/formatter.py
@@ -3,8 +3,8 @@ paragraph_prefix = "--"
 
 
 def format_title(txt, max_w=80):
+def format_title(txt: str, max_w=80, padding=5) -> str:
     # split text by straight number of characters
-    split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
     split_txt = list(map(lambda x: f"{title_prefix} {x}", split_txt))
     split_txt = ["#"*max_w] + split_txt + ["#"*max_w]
     return "\n".join(split_txt).upper()
