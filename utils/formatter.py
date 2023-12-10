f325b1f71458987774d60aef2c5666f2886144de
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-09 21:50:33 -0800

    overwrite same variable

--- utils/formatter.py
+++ utils/formatter.py
@@ -5,10 +5,10 @@ paragraph_prefix = "--"
 def format_title(txt, max_w=80):
     # split text by straight number of characters
     split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
-    split_txt2 = list(map(lambda x: f"{title_prefix} {x}", split_txt))
-    return "\n".join(split_txt2).upper()
+    split_txt = list(map(lambda x: f"{title_prefix} {x}", split_txt))
+    return "\n".join(split_txt).upper()
 
 def format_paragraph(txt, max_w=80):
     split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
-    split_txt2 = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
-    return "\n".join(split_txt2)
+    split_txt = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
+    return "\n".join(split_txt)
