0e561777294a6d55220e14e00d006eb6834f2ffe
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-12-10 00:22:38 -0800

    split text taking padding into account

--- utils/formatter.py
+++ utils/formatter.py
@@ -13,8 +13,8 @@ def format_title(txt: str, max_w=80, padding=5) -> str:
     split_txt = ["#"*max_w] + split_txt + ["#"*max_w]
     return "\n".join(split_txt).upper()
 
-    split_txt = [txt[i:i+max_w] for i in range(0, len(txt), max_w)]
 def format_paragraph(txt: str, max_w=80, padding=2) -> str:
     padding_x = max_w - padding*2
+    split_txt = [txt[i:i+padding_x] for i in range(0, len(txt), padding_x)]
     split_txt = list(map(lambda x: f"{paragraph_prefix} {x}", split_txt))
     return "\n".join(split_txt)
