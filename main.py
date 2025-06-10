526018627475d1bda0903fbe0969a582e97c7a5a
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-06-09 19:40:47 -0700

    loop through scrapers

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from scrapers.nytimes import NYTimes
+from scrapers.washingtonpost import WashingtonPost
 
 def main():
-    scraper = NYTimes()
-    scraper.get_homepage()
+    for Scraper in [NYTimes, WashingtonPost]:
+        scraper = Scraper()
+        scraper.get_homepage()
+        print("")
+        print("")
+        print("")
+        print("")
 
 if __name__ == "__main__":
     main()
